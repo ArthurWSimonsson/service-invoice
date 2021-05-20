e9bd5fd501d8e452eddfa1e0f32d9ffff1f1025e
ArthurWSimonsson <simonsson.arthurw@gmail.com>
2021-05-20 14:22:00 +0200

    changed urls

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -22,8 +22,8 @@ exports.addTransaction = async (invoice, tagName) => {
 // Does a request to the transaction service to delete a transaction.
 exports.removeTransaction = async(invoice) => {
 
-    // return await fetch('http://localhost:3004/api/transaction', {
-    return await fetch('http://transaction:1002/api/transaction', {
+    return await fetch('http://localhost:3004/api/transaction', {
+    // return await fetch('http://transaction:1002/api/transaction', {
     method: 'DELETE',
     headers: {
       'Accept': 'application/json',
@@ -36,8 +36,8 @@ exports.removeTransaction = async(invoice) => {
 // Does a request to the transaction service to change the amount of a transaction
 exports.setTransactionAmount = async(invoice) => {
 
-    // return await fetch('http://localhost:3004/api/transaction/amount/set', {
-    return await fetch('http://transaction:1002/api/transaction/amount/set', {
+    return await fetch('http://localhost:3004/api/transaction/amount/set', {
+    // return await fetch('http://transaction:1002/api/transaction/amount/set', {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
