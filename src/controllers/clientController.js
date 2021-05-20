336eaf16b67d0ea19d2dbb8ee77d93b7f5ae4e59
ArthurWSimonsson <simonsson.arthurw@gmail.com>
2021-05-20 14:41:46 +0200

    changed url:s to env variables

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -1,9 +1,10 @@
 const fetch = require('node-fetch');
+require("dotenv").config();
 
 // Fetches client uuid from client service and adds it to the invoice
 exports.addClientUUID = async (invoice) => {
     try{
-        const result = await fetch(`http://localhost:3003/api/client/${invoice.clientNr}`)
+        const result = await fetch(process.env.CLIENT_URL + invoice.clientNr)
         // const result = await fetch(`http://client:1001/api/client/${invoice.clientNr}`)
         .then(res => res.json())
         .then(res => res)
@@ -20,7 +21,7 @@ exports.addClientUUID = async (invoice) => {
 }
 
 exports.deleteClient = async (invoice) => {
-    return await fetch(`http://localhost:3003/api/client/`, {
+    return await fetch(process.env.CLIENT_URL, {
         method: 'DELETE',
         headers: {
           'Accept': 'application/json',
--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -1,9 +1,10 @@
 const fetch = require('node-fetch');
+require("dotenv").config();
+
 // Does a request to the transaction service to add a transaction.
 exports.addTransaction = async (invoice, tagName) => {
 
-    let res = await fetch('http://localhost:3004/api/transaction', {
-    // await fetch('http://transaction:1002/api/transaction', {
+    let res = await fetch(process.env.TRANSACTION_URL, {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
@@ -22,8 +23,7 @@ exports.addTransaction = async (invoice, tagName) => {
 // Does a request to the transaction service to delete a transaction.
 exports.removeTransaction = async(invoice) => {
 
-    return await fetch('http://localhost:3004/api/transaction', {
-    // return await fetch('http://transaction:1002/api/transaction', {
+    return await fetch(process.env.TRANSACTION_URL, {
     method: 'DELETE',
     headers: {
       'Accept': 'application/json',
@@ -36,8 +36,7 @@ exports.removeTransaction = async(invoice) => {
 // Does a request to the transaction service to change the amount of a transaction
 exports.setTransactionAmount = async(invoice) => {
 
-    return await fetch('http://localhost:3004/api/transaction/amount/set', {
-    // return await fetch('http://transaction:1002/api/transaction/amount/set', {
+    return await fetch(process.env.TRANSACTION_URL + 'amount/set', {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
